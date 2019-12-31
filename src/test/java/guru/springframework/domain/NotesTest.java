d61ec321afebea87043f761751706a7cf6305ee3
Krzysztof Kukla <krzys.kukla@gmail.com>
2019-12-31 21:33:05 +0100

    96 JUnit 4 & 5 together

---
+++ src/test/java/guru/springframework/domain/NotesTest.java
@@ -0,0 +1,16 @@
+package guru.springframework.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Krzysztof Kukla
+ */
+class NotesTest {
+
+    @Test
+    void setId() {
+    }
+
+}
